9cb82b858b73818551681ee0220ed5688d6fede4
Kirill <bloodwingest@gmail.com>
2022-07-12 18:40:24 +0300

    fix: fixed burger script

--- travel/burger.js
+++ travel/burger.js
@@ -1,11 +1,20 @@
 function burgerFunction() {
     const nav = document.getElementById('nav');
-document.querySelector('.burger-menu').addEventListener("click", function(){
+    document.querySelector('.burger-menu').addEventListener("click", function(){
         nav.style.right = "0px";
     })
-document.querySelector('.close').addEventListener("click", function(){
+    document.querySelector('.close').addEventListener("click", function(){
         nav.style.right = "-200px";
     })
+    /*document.querySelector('.anchors').addEventListener("click", function(){
+        nav.style.right = "-200px";
+    })*/
+    const anchors = document.querySelectorAll('.anchors');
+    for (let i = 0; i < anchors.length; i++){
+        anchors[i].addEventListener("click", function(){
+            nav.style.right = "-200px";
+        })
+    }
 }
 
 burgerFunction();
