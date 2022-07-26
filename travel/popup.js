d5c39ae0c409e71b1ec86b3ae9bcecfaeb6a2d27
Kirill <bloodwingest@gmail.com>
2022-07-26 20:46:12 +0300

    feat: added input alert

--- travel/popup.js
+++ travel/popup.js
@@ -1,1 +1,23 @@
-const loginButton = document.querySelector
+const loginButton = document.querySelector('.login');
+const popup = document.querySelector('.pop-up');
+const email = document.querySelector('.pop-up-input')
+const password = document.querySelector('.password')
+const signInButton = document.querySelector('.sign-in');
+
+loginButton.addEventListener('click',() => {
+    popup.classList.toggle('hidden')
+})
+
+popup.addEventListener(('click'), (event) =>{
+    if(event.target.classList.contains('pop-up')){
+        popup.classList.toggle('hidden')
+    }
+})
+
+signInButton.addEventListener('click', () =>{
+    let userEmail = document.querySelector('.pop-up-input').value;
+    let userPassword = password.value;
+    alert(`Your Email: ${userEmail}\nYour Password: ${userPassword}`)
+})
+
+
